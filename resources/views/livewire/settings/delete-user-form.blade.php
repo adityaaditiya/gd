e732d0d10adb3803213ff603f3f7c638468eaa69
adityaaditiya <adityamuhammad9@gmail.com>
2025-11-08 17:22:54 +0700

    tombol tambah nasabah

--- resources/views/livewire/settings/delete-user-form.blade.php
+++ resources/views/livewire/settings/delete-user-form.blade.php
@@ -24,13 +24,13 @@ new class extends Component {
 
 <section class="mt-10 space-y-6">
     <div class="relative mb-5">
-        <flux:heading>{{ __('Delete account') }}</flux:heading>
-        <flux:subheading>{{ __('Delete your account and all of its resources') }}</flux:subheading>
+        <flux:heading>{{ __('Hapus akun') }}</flux:heading>
+        <flux:subheading>{{ __('Hapus akun Anda dan semua sumber dayanya') }}</flux:subheading>
     </div>
 
     <flux:modal.trigger name="confirm-user-deletion">
         <flux:button variant="danger" x-data="" x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')" data-test="delete-user-button">
-            {{ __('Delete account') }}
+            {{ __('Hapus akun') }}
         </flux:button>
     </flux:modal.trigger>
 
