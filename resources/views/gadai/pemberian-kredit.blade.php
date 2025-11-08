3b54e83846608b5b7afafe78d1593dc2a03855ce
adityaaditiya <48347372+adityaaditiya@users.noreply.github.com>
2025-11-08 17:26:22 +0700

    Remove Tambah Nasabah sidebar submenu

---
+++ resources/views/gadai/pemberian-kredit.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app :title="__('Pemberian Kredit')">
+    <div class="flex flex-col gap-4">
+        <h1 class="text-2xl font-semibold text-neutral-900 dark:text-white">{{ __('Pemberian Kredit') }}</h1>
+        <p class="text-neutral-600 dark:text-neutral-300">
+            {{ __('Halaman ini merupakan tempat untuk mengelola proses pemberian kredit gadai.') }}
+        </p>
+    </div>
+</x-layouts.app>
