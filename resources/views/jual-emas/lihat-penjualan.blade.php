6eeda7ff02f803069e85ad9b04ade2330f37875b
adityaaditiya <48347372+adityaaditiya@users.noreply.github.com>
2025-11-08 13:53:04 +0700

    Fix Data Nasabah modal initialization and controls

---
+++ resources/views/jual-emas/lihat-penjualan.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app :title="__('Lihat Penjualan')">
+    <div class="flex flex-col gap-4">
+        <h1 class="text-2xl font-semibold text-neutral-900 dark:text-white">{{ __('Lihat Penjualan') }}</h1>
+        <p class="text-neutral-600 dark:text-neutral-300">
+            {{ __('Tinjau daftar penjualan emas yang telah tercatat.') }}
+        </p>
+    </div>
+</x-layouts.app>
