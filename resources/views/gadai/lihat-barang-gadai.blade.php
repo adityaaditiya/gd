c474e2ac51104f52316dc73bcd7ecc28ddc6e08e
adityaaditiya <48347372+adityaaditiya@users.noreply.github.com>
2025-11-09 13:10:32 +0700

    Add electronics pawn menu entry for item listing

---
+++ resources/views/gadai/lihat-barang-gadai.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app :title="__('Lihat Barang Gadai')">
+    <div class="flex flex-col gap-4">
+        <h1 class="text-2xl font-semibold text-neutral-900 dark:text-white">{{ __('Lihat Barang Gadai') }}</h1>
+        <p class="text-neutral-600 dark:text-neutral-300">
+            {{ __('Halaman ini menampilkan daftar barang elektronik yang sedang digadaikan.') }}
+        </p>
+    </div>
+</x-layouts.app>
